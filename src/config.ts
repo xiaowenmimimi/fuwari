59910124900fa46fecac7db3c0b5d680b03a1474
xiaohw <xiaowenmimimi@163.com>
2025-12-09 14:50:39 +0800

    Fuwari 主题基本信息配置

--- src/config.ts
+++ src/config.ts
@@ -8,9 +8,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "Wen",
+	subtitle: "一个基于 Astro 的静态博客 Fuwari 主题",
+	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		// 未手动设置主题色时, 网站主题色会随机变为 黄色(100)、蓝色(250)、青色(200) 或 粉色(345) 中的一种
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -19,12 +19,16 @@ export const siteConfig: SiteConfig = {
 	banner: {
 		enable: true,
 		// 支持多张图片轮播的数组配置
-		src: ["assets/images/demo-banner.png"], // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: [
+			"assets/images/demo-banner.png",
+			"assets/images/demo-banner.png",
+			"assets/images/demo-banner.png",
+		], // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: true, // Display the credit text of the banner image
-			text: ["Author 1"], // Credit text to be displayed
-			url: [""], // (Optional) URL link to the original artwork or artist's page
+			text: ["Author 1", "Author 2", "Author 3"], // Credit text to be displayed
+			url: ["", "", ""], // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
@@ -46,35 +50,40 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Home,
 		LinkPreset.Archive,
 		LinkPreset.About,
-		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
-		},
+		// {
+		// 	name: "GitHub",
+		// 	url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+		// 	external: true, // Show an external link icon and will open in a new tab
+		// },
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/head_portrait.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "朗姆提子冰淇淋",
+	bio: "因为喜欢所以创建了博客\nCiallo～(∠・ω< )⌒☆",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
+		// {
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it's not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
+		// },
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/profiles/76561198855701189/",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/xiaowenmimimi/fuwari",
+		},
+		{
+			name: "Email",
+			icon: "fa6-solid:envelope",
+			url: "mailto:1847559091@qq.com",
 		},
 	],
 };
