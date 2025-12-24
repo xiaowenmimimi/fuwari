d2a2e048218753ace8c73f0f489cb351330f3eaf
xiaohw <xiaowenmimimi@163.com>
2025-12-24 11:08:04 +0800

    修改 site 配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://blog.xhwen.cn/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
